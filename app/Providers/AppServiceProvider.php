53f81cf9ad7e9b05ce0da4d335927860bb9fa330
kikorobledo <enrique_j_@hotmail.com>
2024-07-08 13:05:54 -0600

    Fix s3

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,12 @@
 namespace App\Providers;
 
 use Livewire\Livewire;
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -15,7 +18,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+
     }
 
     /**
@@ -31,11 +34,11 @@ class AppServiceProvider extends ServiceProvider
             URL::forceScheme('https');
 
             Livewire::setScriptRoute(function ($handle) {
-                return Route::get('/sarchivo/public/vendor/livewire/livewire.js', $handle);
+                return Route::get('/sarchivo/public/vendor/livewire/livewire.js', $handle)->middleware('web');
             });
 
             Livewire::setUpdateRoute(function ($handle) {
-                return Route::post('/sarchivo/public/livewire/update', $handle);
+                return Route::post('/sarchivo/public/livewire/update', $handle)->middleware('web');
             });
 
         }elseif(env('LOCAL') === "0"){
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,8 +22,6 @@ return Application::configure(basePath: dirname(__DIR__))
             'permission' => PermissionMiddleware::class
         ]);
 
-        $middleware->trustProxies('*');
-
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
