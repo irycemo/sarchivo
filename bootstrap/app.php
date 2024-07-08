f2f4405bc12b471e5924060c09f7d76b71a422a5
kikorobledo <enrique_j_@hotmail.com>
2024-07-08 12:45:53 -0600

    Fix s3

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,6 +22,8 @@ return Application::configure(basePath: dirname(__DIR__))
             'permission' => PermissionMiddleware::class
         ]);
 
+        $middleware->trustProxies('*');
+
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
