b52e4cee861fb8ba096b90d00adf03f934c4333d
root <root@DESKTOP-IVHFQKO.localdomain>
2026-08-20 10:30:33 -0600

    Fix logs

--- config/log-viewer.php
+++ config/log-viewer.php
@@ -72,8 +72,9 @@ return [
     */
 
     'middleware' => [
-        //'web',
-        //\Opcodes\LogViewer\Http\Middleware\AuthorizeLogViewer::class,
+        'web',
+        'auth',
+        \Opcodes\LogViewer\Http\Middleware\AuthorizeLogViewer::class,
     ],
 
     /*
@@ -86,8 +87,9 @@ return [
     */
 
     'api_middleware' => [
-        //\Opcodes\LogViewer\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-        //\Opcodes\LogViewer\Http\Middleware\AuthorizeLogViewer::class,
+        'web',
+        \Opcodes\LogViewer\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+        \Opcodes\LogViewer\Http\Middleware\AuthorizeLogViewer::class,
     ],
 
     'api_stateful_domains' => env('LOG_VIEWER_API_STATEFUL_DOMAINS') ? explode(',', env('LOG_VIEWER_API_STATEFUL_DOMAINS')) : null,
