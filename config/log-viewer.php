1cadce7c0df67e18817acbc9b24ec073ccd09785
root <root@DESKTOP-IVHFQKO.localdomain>
2026-08-20 10:17:54 -0600

    Fix logs

--- config/log-viewer.php
+++ config/log-viewer.php
@@ -14,7 +14,7 @@ return [
 
     'api_only' => env('LOG_VIEWER_API_ONLY', false),
 
-    'require_auth_in_production' => false,
+    'require_auth_in_production' => true,
 
     /*
     |--------------------------------------------------------------------------
@@ -74,7 +74,7 @@ return [
     'middleware' => [
         'web',
         'auth',
-        \Opcodes\LogViewer\Http\Middleware\AuthorizeLogViewer::class,
+        //\Opcodes\LogViewer\Http\Middleware\AuthorizeLogViewer::class,
     ],
 
     /*
